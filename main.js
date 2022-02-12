8e2cc111e29b9f76ce296c91de4fe69cf69a946a
Victor Cobo <v.cobof@gmail.com>
2022-02-12 12:05:28 -0300

    finished all lessons

--- main.js
+++ main.js
@@ -18,17 +18,17 @@ for (const link of links) {
 }
 
 // adicionar sombra ao header quando descer
-
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
-  if (this.window.scrollY >= navHeight) {
+function addShadowToHeader() {
+
+  if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 // Testimonials Carousel with Swiper
 const swiper = new Swiper('.swiper-container', {
@@ -37,8 +37,14 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
-});
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
 
 // ScrollReveal : show elements when scrolling through the page
 const scrollReveal = ScrollReveal({
@@ -53,6 +59,53 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
-  `, 
-  {interval: 100})
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+// Return to the top button 
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// keep menu activated according to current section
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.scrollY + (window.innerHeight / 8) * 4 // 8 and 4 just worked. There's no explanation for them
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop // position of the section top
+    const sectionHeight = section.offsetHeight // section height
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active')
+    } 
+    else {
+      document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active')
+    }
+  }
+
+}
+
+
+// scroll related features
+window.addEventListener('scroll', function () {
+  addShadowToHeader()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
+
+
